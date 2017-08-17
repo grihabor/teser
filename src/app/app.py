ef59fe48da6f08185671ed2c09a335a1ebffcb82
Borodin Gregory <grihabor@mail.ru>
2017-08-17 11:18:33 +0300

    create basic template structure

--- src/app/app.py
+++ src/app/app.py
@@ -1,13 +1,21 @@
+import shutil
 import logging
 import os
-from flask import Flask
+
+from flask import Flask, render_template
 from flask_mail import Mail
 from flask_security import (
-    Security, login_required, SQLAlchemySessionUserDatastore
+    Security, login_required, SQLAlchemySessionUserDatastore,
+    current_user
 )
 from database import db_session, init_db
 from models import User, Role
 
+
+DIR_APP = os.path.split(os.path.abspath(__file__))[0]
+DIR_TEMPLATES = os.path.join(DIR_APP, 'templates')
+FILE_BASE_HTML = os.path.join(DIR_TEMPLATES, 'base.html')
+FILE_BASE_HTML_EXAMPLE = os.path.join(DIR_TEMPLATES, 'base.html.example')
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -37,32 +45,28 @@ user_datastore = SQLAlchemySessionUserDatastore(db_session, User, Role)
 security = Security(app, user_datastore)
 
 
-# Create a user to test with
-@app.before_first_request
-def create_user():
-    init_db()
-
-    kwargs = dict(
-        email='example@gmail.com',
-        password='password',
-    )
-
-    if not user_datastore.find_user(**kwargs):
-        user_datastore.create_user(**kwargs)
-        db_session.commit()
-
 # Views
 @app.route('/')
 @login_required
 def home():
-    return 'Here you go!'
+    logger.info('Current user: {}'.format(current_user))
+    return render_template('index.html')
 
-if __name__ == '__main__':
+def maybe_create_base():
+    if not os.path.exists(FILE_BASE_HTML):
+        shutil.copy(FILE_BASE_HTML_EXAMPLE, FILE_BASE_HTML)
+
+
+def main():
     host = os.getenv('FLASK_HOST', '127.0.0.1')
     port = int(os.getenv('FLASK_PORT', 5000))
 
     kwargs = dict(host=host, port=port)
     logger.info('Flask config: {}'.format(kwargs))
+
+    maybe_create_base()
     app.run(**kwargs)
 
-    app.run(host=host, port=port)
+
+if __name__ == '__main__':
+    main()
