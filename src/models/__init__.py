536b7fac49dd690a40b9a6c3e74e6cd7adadaaa9
Borodin Gregory <grihabor@mail.ru>
2017-08-18 10:52:31 +0300

    repository db model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,11 @@
+from .repository import Repository
 from .role import Role
 from .user import User
 from .roles_users import RolesUsers
+
+__all__ = [
+    'User',
+    'Repository',
+    'RolesUsers',
+    'Role',
+]
---
+++ src/models/repository.py
@@ -0,0 +1,12 @@
+from database import Base
+from sqlalchemy import (
+    Integer, String, ForeignKey, Column
+)
+
+
+class Repository(Base):
+    __tablename__ = 'repository'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    url = Column(String(2000), nullable=False)  # https://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers
+
