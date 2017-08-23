fcec7f4e244dcd5f256e31e1f5b9befe49e98e09
Borodin Gregory <grihabor@mail.ru>
2017-08-23 12:58:57 +0300

    parse repo url

--- src/app/init/__init__.py
+++ src/app/init/__init__.py
@@ -4,14 +4,13 @@ import sys
 import shutil
 
 from database import POSTGRES_URL
+from util import DIR_SRC
 
-DIR_INIT = os.path.split(os.path.abspath(__file__))[0]
-DIR_APP = os.path.normpath(os.path.join(DIR_INIT, os.pardir))
+DIR_APP = os.path.join(DIR_SRC, 'app')
 DIR_TEMPLATES = os.path.join(DIR_APP, 'templates')
 FILE_BASE_HTML = os.path.join(DIR_TEMPLATES, 'base.html')
 FILE_BASE_HTML_EXAMPLE = FILE_BASE_HTML + '.example'
 
-DIR_SRC = os.path.normpath(os.path.join(DIR_APP, os.pardir))
 FILE_ALEMBIC_INI = os.path.join(DIR_SRC, 'alembic.ini')
 FILE_ALEMBIC_INI_EXAMPLE = FILE_ALEMBIC_INI + '.example'
 
--- src/tester/app.py
+++ src/tester/app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+import os
+import subprocess
+from urllib.parse import urlparse, ParseResult
+
+from flask import Flask, request, jsonify
+
+from util import DIR_ROOT
 
 app = Flask(__name__)
+
+WORKDIR = os.path.join(DIR_ROOT, 'workdir')
+ARG_URL = 'url'
 
 
 @app.route('/')
@@ -8,13 +17,34 @@ def index():
     return 'Internal tester service'
 
 
-def parse_url(url):
-    
+def parse_repo_url(url):
+    o = urlparse(url)  # type: ParseResult
+    return dict(username=o.username if o.username else 'git',
+                hostname=o.hostname,
+                path=o.path)
 
 
-@app.route('/clone_repo/<url>/<key>')
-def clone_repo(url, key):
-    subprocess.run("
+@app.route('/clone_repo')
+def clone_repo():
+    if ARG_URL not in request.args:
+        return jsonify(dict(details='missing_url', ok=0))
+
+    url = request.args[ARG_URL]
+    parsed = parse_repo_url(url)
+
+    if parsed['hostname'] is None:
+        return jsonify(dict(ok=0, details='missing_hostname'))
+
+    command = ['git',
+               'clone',
+               '{username}@{hostname}:{path}'.format(**parsed)]
+
+    print('Command: {}'.format(' '.join(command)))
+
+    completed = subprocess.run(command, cwd=WORKDIR)
+    return jsonify(dict(ok=(completed.returncode == 0),
+                        returncode=completed.returncode))
+
 
 def main():
     app.run('0.0.0.0', 6000, debug=True)
