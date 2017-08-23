bae5ceb2096306a38bce9dd0b9d74ba8191ef98a
Borodin Gregory <grihabor@mail.ru>
2017-08-23 17:48:23 +0300

    better logging

--- src/tester/clone.sh
+++ src/tester/clone.sh
@@ -2,4 +2,10 @@
 
 eval `ssh-agent`
 ssh-add {identity_file}
-git clone {user}@{host}:{path}
+if [ $? -eq 0 ]; then
+    echo "ssh-add: ok"
+    git clone {user}@{host}:{path}
+else
+    echo "ssh-add: fail"
+fi
+
