73fd7ee5af7d44e38d11ee9c0f7546971f6979a9
Borodin Gregory <grihabor@mail.ru>
2017-08-25 00:29:33 +0300

    nice user experience when trying to clone repo

--- src/app/static/home.js
+++ src/app/static/home.js
@@ -1,15 +1,22 @@
+
+var failed_to_clone_text = 'Failed to clone the repository';
+var failed_to_clone = $('<p class="help-block" id="failed_to_clone">' + failed_to_clone_text + '</p>');
+
 function add_repository(url, deploy_key_container, on_success) {
     var request = $.get("/add_repository", {"url": url.val()});
 
     request.success(function (response) {
+        var url_container = url.parent();
+
         console.log('Add repo');
         console.log(response);
 
         if(response.result == 'invalid repository') {
-            var url_container = url.parent();
             url_container.addClass('has-error');
+            failed_to_clone.insertAfter(url);
         } else {
             url.val("");
+            url_container.removeClass('has-error');
             deploy_key_container.hide();
             on_success();
         }
@@ -48,15 +55,23 @@ $(function () {
     var state = 0,
         deploy_key = $("#deploy_key"),
         deploy_key_container,
-        url = $("#url");
+        url = $("#url"),
+        url_container;
 
     deploy_key_container = deploy_key.parent();
+    url_container = url.parent();
 
     deploy_key.attr('readonly', 'readonly');
     deploy_key_container.hide();
 
     function toggle_state() {state = 1 - state;}
 
+    url.on('keyup paste', function () {
+        if (state === 1) {
+            failed_to_clone.remove();
+            url_container.removeClass('has-error')
+        }
+    });
 
     $('#add_repository').submit(function (e) {
         e.preventDefault();
