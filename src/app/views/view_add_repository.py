7339bcbf6896f82feb393b9321b0f6ab697b0ab7
Borodin Gregory <grihabor@mail.ru>
2017-08-24 23:00:03 +0300

    move add_repository view to separate file

---
+++ src/app/views/view_add_repository.py
@@ -0,0 +1,57 @@
+import json
+import urllib
+
+import logging
+
+from flask import request, jsonify
+from flask_security import login_required, current_user
+
+from app import app
+from database import db_session
+from models import Repository
+
+logger = logging.getLogger(__name__)
+
+
+def validate_repository(url, identity_file):
+    path = 'http://tester/clone_repo?url={}&identity_file={}'.format(
+        url, identity_file
+    )
+
+    with urllib.request.urlopen(path) as r:
+        content = r.read()
+        logger.info(content)
+        obj = json.loads(content)
+    return obj.ok
+
+
+@app.route('/add_repository')
+@login_required
+def add_repository():
+    url = request.args['url']
+    identity_file = current_user.generated_identity_file
+
+    if identity_file is None:
+        return "", 500
+
+    if validate_repository(url, identity_file):
+        repo = Repository(user_id=current_user.id,
+                          url=url,
+                          identity_file=identity_file)
+        try:
+            db_session.add(repo)
+            current_user.generated_identity_file = None
+            db_session.commit()
+            result = 'ok'
+        except Exception as e:
+            logger.warning(e)
+            db_session.rollback()
+            result = 'invalid repository'
+    else:
+        result = 'invalid repository'
+
+    return jsonify(dict(
+        result=result,
+        repositories=[dict(url=repo.url)
+                      for repo in current_user.repositories]
+    ))
