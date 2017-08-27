3fb83997d9137034fab8deaa4a6bb837bb00819c
Borodin Gregory <grihabor@mail.ru>
2017-08-27 21:37:30 +0300

    move home.js file

--- src/app/static/js/home/index.js
+++ src/app/static/js/home/index.js
@@ -68,6 +68,35 @@ function RepositoryList(props) {
     )
 }
 
+function RepositoryDeployKey(props) {
+
+}
+
+function RepositorySubmit(props) {
+    return (
+            <input className="btn btn-default" id="submit_button" name="submit_button" value="Add" type="submit" />
+    )
+}
+
+function RepositoryAddForm(props) {
+    return (
+        <form id="add_repository">
+            <RepositoryURL />
+            <RepositoryDeployKey />
+            <RepositorySubmit />
+        </form>
+    )
+}
+
+function RepositoryAdd(props) {
+    return (
+        <div>
+            <h2 className="header">Add repository</h2>
+            <RepositoryAddForm />
+        </div>
+    )
+}
+
 function HomePage(props) {
     return (
         <div id="page_content">
