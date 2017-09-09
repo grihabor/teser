0f7d7a712521c2cf996d19b7335e7b9cb4a3d233
Borodin Gregory <grihabor@mail.ru>
2017-09-09 10:42:12 +0300

    uset tab view

--- src/app/static/js/admin_page/index.jsx
+++ src/app/static/js/admin_page/index.jsx
@@ -53,7 +53,11 @@ class AdminPage extends React.Component{
         this.state = {
             tab: 'user_list'
         }
-        tabs = {
+        
+    }
+    
+    render() {
+        const tabs = {
             user_list: {
                 id: 'user_list',
                 content: <UserList />
@@ -67,57 +71,15 @@ class AdminPage extends React.Component{
                 id: 'smth',
                 content: <div>smth</div>
             }
-        }
-                
-        
-        
-        
-        this.showTestingPanel = this.showTestingPanel.bind(this);
-        this.showUserList = this.showUserList.bind(this);
-        this.showActiveRepositoryList = this.showActiveRepositoryList.bind(this);
-    }
-    showUserList() {
-        this.setState({tab: 'user_list'});
-    }
-    showTestingPanel() {
-        this.setState({tab: 'testing_panel'});
-    }
-    showActiveRepositoryList() {
-        this.setState({tab: 'active_repository_list'});
-    }
-    render() {
-        let tab_content;
-        if (this.state.tab === 'user_list') {
-            tab_content = <UserList />;
-        } else if (this.state.tab === 'testing_panel'){
-            tab_content = <TestingPanel />;
-        } else {
-            tab_content = <ActiveRepositoryList />;
-        }
+        };
         return (
-        <div>
-            <p className="screen-width">
-                Back to <a href={this.props.home_page}>Home page</a>
-            </p>
+            <div>
+                <p className="screen-width">
+                    Back to <a href={this.props.home_page}>Home page</a>
+                </p>
             
-            <div id="page_content">
-                <h1 className="header screen-width">
-                    Admin Page
-                </h1>
-                <div>
-                <button onClick={this.showUserList}>
-                    User List
-                </button>
-                <button onClick={this.showActiveRepositoryList}>
-                    Active Repository List
-                </button>
-                <button onClick={this.showTestingPanel}>
-                    Testing Panel
-                </button>
-                </div>
-                {tab_content}
+                <TabView tabs={tabs} />
             </div>
-        </div>
         )
     }
 }
