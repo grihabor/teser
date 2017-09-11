403b75c63175d853b994442c50872d1c741ee17b
Borodin Gregory <grihabor@mail.ru>
2017-09-11 14:25:31 +0300

    preserve tab order

--- src/app/static/js/admin_page/index.jsx
+++ src/app/static/js/admin_page/index.jsx
@@ -1,19 +1,19 @@
 function AdminPage(props) {
-    const tabs = {
-        user_list: {
+    const tabs = [
+        {
             id: 'user_list',
             title: 'User List',
             content: <UserList />
-        }, testing_panel: {
+        }, {
             id: 'testing_panel',
             title: 'Testing Panel',
             content: <TestingPanel />
-        }, active_repository_list: {
+        }, {
             id: 'active_repository_list',
             title: 'Active Repositories',
             content: <ActiveRepositoryList />
         }
-    };
+    ];
     return (
         <div>
             <p className="screen-width">
@@ -22,7 +22,7 @@ function AdminPage(props) {
 
             <TabView
                 tabs={tabs}
-                initial_tab={tabs.user_list}/>
+                initial_tab={tabs[0]}/>
         </div>
     )
 
