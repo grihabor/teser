feb3bc10e50cd35b3a041b75d9c0fd767333fa7c
Borodin Gregory <grihabor@mail.ru>
2017-09-13 12:50:56 +0300

    refactor: move api into api folder

--- src/app/views/api/__init__.py
+++ src/app/views/api/__init__.py
@@ -1,4 +1,11 @@
 from .task import import_task
+from .deploy_key import import_generate_deploy_key
+from .repository import import_repository
+from .user import import_user
+
 
 def import_api(app):
     import_task(app)
+    import_generate_deploy_key(app)
+    import_user(app)
+    import_repository(app)
