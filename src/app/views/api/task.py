0a92d829ebdb3fc74548bd23ceac95499fa5bd54
Borodin Gregory <grihabor@mail.ru>
2017-09-13 12:26:26 +0300

    ask tester service to run tests

--- src/app/views/api/task.py
+++ src/app/views/api/task.py
@@ -1,18 +1,14 @@
-import json
 import logging
+
 import urllib
 
-from flask import request, jsonify
-from flask_security import login_required, current_user, roles_required
+import json
+from flask import jsonify
+from flask_security import login_required\
 
-from database import db_session
-from models import Repository, User
 from util import safe_get_repository
-from util.details import process_details
-from util.exception import UIError
 from util.unified_response import UnifiedResponse
 
-from sqlalchemy.orm import join
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -23,6 +19,9 @@ def import_task(app):
     @login_required
     def task_start():
         repo = safe_get_repository('repository_id')
-        r = UnifiedResponse(result='fail', details='Not implemented')
-        return jsonify(dict(r))
 
+        url = f'http://tester:6000/run_tests?repository_id={repo.id}&user_id={repo.user_id}'
+        with urllib.request.urlopen(url) as f:
+            response = f.read()
+
+        return response
