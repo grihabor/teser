81f3d5509ff651bb5bb5db6930de3a19a09ddad8
Borodin Gregory <grihabor@mail.ru>
2017-09-28 19:57:05 +0300

    make run_tests a task

--- src/tasks.py
+++ src/tasks.py
@@ -1,10 +1,27 @@
 from celery_app import app
 
 
-@app.task
-def run_tests(repo_id, user_id):
-    url = f'http://tester:6000/run_tests?repository_id={repo_id}&user_id={user_id}'
-    with urllib.request.urlopen(url) as f:
-        response = f.read()
-    return response
-    
+@app.task(name='tasks.tests.run')
+def tests_run(repo_id):
+    repo = Repository.get(repo_id)
+    kwargs = parse_repo_url(repo.url)
+    if kwargs is None:
+        return dict(UnifiedResponse(
+            result='fail',
+            details='Invalid repository'
+        ))
+
+    result = run_bash_script(
+        FILE_TEST_SH,
+        identity_file=repo.identity_file,
+        git_template=Git(path='/grihabor/compressor',
+                         user='git',
+                         host='gitlab.com'),
+        git=Git(**kwargs)
+    )
+
+    if 'debug' in request.args:
+        result.details = process_details(result.details)
+        return render_template('debug.html', **dict(result))
+
+    return jsonify(dict(result))
