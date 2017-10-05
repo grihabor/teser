a7febd2758684d6f9c5ec5fcd1f7b4dd45680f75
Borodin Gregory <grihabor@mail.ru>
2017-10-05 11:02:32 +0300

    try abs import

--- src/tasks.py
+++ src/tasks.py
@@ -1,35 +1,11 @@
 from celery_app import app
-from models import Repository
+import models 
 
 
-
-class Git:
-    def __init__(self, user, host, path):
-        self.path = path
-        self.user = user
-        self.host = host
-        
-        
-def parse_repo_url(url):
-    """Format: {user}@{host}:{path}"""
-
-    result = url.split(':')
-    if len(result) != 2:
-        return
-
-    hostinfo, path = result
-    result = hostinfo.split('@')
-    if len(result) != 2:
-        return
-
-    user, host = result
-    return Git(user=user,
-               host=host,
-               path=path)
 
 @app.task(name='tasks.tests.run')
 def run_tests(repo_id):
-    repo = Repository.get(repo_id)
+    repo = models.Repository.get(repo_id)
     kwargs = parse_repo_url(repo.url)
     if kwargs is None:
         return dict(
