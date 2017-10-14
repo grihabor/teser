e5e82707f2aa70c7f76f81d672a26d24c77a80b5
Borodin Gregory <grihabor@mail.ru>
2017-10-14 23:53:27 +0300

    fix task param

--- src/app/views/api/deploy_key.py
+++ src/app/views/api/deploy_key.py
@@ -17,7 +17,7 @@ def import_generate_deploy_key(app):
     @app.route('/api/deploy_key/generate', methods=['GET'])
     @login_required
     def generate_deploy_key_endpoint():
-        result = generate_deploy_key.delay()
+        result = generate_deploy_key.delay(current_user.id)
         logger.info('Waiting for deploy key to generate...')
         deploy_key = result.get()
         return jsonify(dict(deploy_key=deploy_key, result='ok'))
