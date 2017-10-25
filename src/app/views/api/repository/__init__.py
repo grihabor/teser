23d0743e325da1eb59fdd2f6ed1295e07c648cc7
Borodin Gregory <grihabor@mail.ru>
2017-10-26 00:13:14 +0300

    move repo add to separate file

--- src/app/views/api/repository/__init__.py
+++ src/app/views/api/repository/__init__.py
@@ -17,64 +17,10 @@ from utils import (
     UnifiedResponse
 )
 
+from .add import import_repository_add
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-
-def validate_repository(url, identity_file):
-
-    result = clone_repository.delay(url, identity_file)
-    unified_response = result.get()
-
-    logger.info(unified_response)
-    return unified_response
-
-
-def _add_repository(url):
-    identity_file = current_user.generated_identity_file
-
-    if identity_file is None:
-        return UnifiedResponse(
-            result='fail',
-            details="Current user doesn't have generated identity file"
-        )
-
-    validation = validate_repository(url, identity_file)
-
-    if validation.result == 'ok':
-        repo = Repository(user_id=current_user.id,
-                          url=url,
-                          identity_file=identity_file)
-        try:
-            db_session.add(repo)
-            current_user.generated_identity_file = None
-            db_session.commit()
-            response = UnifiedResponse(
-                result='ok',
-                details=''
-            )
-        except Exception as e:
-            logger.warning(e)
-            db_session.rollback()
-            response = UnifiedResponse(
-                result='fail',
-                details='Failed to save the repository into database'
-            )
-    else:
-        response = UnifiedResponse(
-            result='fail',
-            details=['Failed to validate the url:'] + process_details(validation.details)
-        )
-
-    d = dict(response)
-    d.update(dict(
-        repositories=[dict(
-            id=repo.id,
-            url=repo.url,
-            identity_file=repo.identity_file
-        ) for repo in current_user.repositories]
-    ))
-    return d
 
 
 def maybe_remove_active_repo_ref(repo):
@@ -127,12 +73,7 @@ def active_repositories():
 
 
 def import_repository(app):
-    @app.route('/api/repository/add')
-    @login_required
-    def repository_add():
-        url = request.args['url']
-        return jsonify(dict(_add_repository(url)))
-
+    import_repository_add(app)
     @app.route('/api/repository/list')
     @login_required
     def repository_list():
