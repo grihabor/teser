7f2b5d8bd9e8d61f7e91fe41f9d2d83614a72df3
Borodin Gregory <grihabor@mail.ru>
2017-11-09 15:39:15 +0300

    user `user_repo`directory to clone repo

--- src/tasks/clone.sh
+++ src/tasks/clone.sh
@@ -6,5 +6,5 @@ SERVER_KEY=$(ssh-keyscan -t rsa {git.host})
 mkdir -p ~/.ssh/
 if ! grep -Fxq "$SERVER_KEY" ~/.ssh/known_hosts; then echo $SERVER_KEY >> ~/.ssh/known_hosts; fi
 
-git clone {git.user}@{git.host}:{git.path}
-rm -rf {repository_name}
+git clone {git.user}@{git.host}:{git.path} user_repo
+rm -rf user_repo
